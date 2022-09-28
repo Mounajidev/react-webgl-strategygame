f7a220499f899b0383051c3b83b47f00a9273b01
Mounajidev <lucilo@gmail.com>
2022-09-28 11:30:52 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
+import { Unity, useUnityContext } from "react-unity-webgl";
+
 import './App.css';
 
 function App() {
+
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/WebGL NoCompresion.loader.js",
+    dataUrl: "build/WebGL NoCompresion.data",
+    frameworkUrl: "build/WebGL NoCompresion.framework.js",
+    codeUrl: "build/WebGL NoCompresion.wasm",
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <t1 className="Text-tittle"> Turn Based Strategy Game Prototype </t1>
       </header>
+
+      <div className='Unity-container'>
+
+      <Unity unityProvider={unityProvider} style={{ width: 1280, height: 740 }} />
+      </div>
     </div>
   );
 }
